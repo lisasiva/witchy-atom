4fc1ff05124cf2d56875206162fb07cd4cea237e
lisasiva <lisashantisiva@gmail.com>
2019-11-13 15:35:08 -0800

    Updates

--- precache-manifest.c07accc31cbf0ff7831647b9689a894c.js
+++ precache-manifest.c07accc31cbf0ff7831647b9689a894c.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8260aaa92a6b11d8ad7f408f95a7b709",
+    "revision": "b6ba5f39feb006496416eb73a72de798",
     "url": "/index.html"
   },
   {
-    "revision": "376e04443b6a783284f4",
-    "url": "/static/css/main.163ec2ed.chunk.css"
+    "revision": "3e4024310a7c1b6e7d81",
+    "url": "/static/css/main.5135b26a.chunk.css"
   },
   {
     "revision": "030b1fdf0634124847ab",
     "url": "/static/js/2.48bf6283.chunk.js"
   },
   {
-    "revision": "376e04443b6a783284f4",
+    "revision": "3e4024310a7c1b6e7d81",
     "url": "/static/js/main.13014a7b.chunk.js"
   },
   {
     "revision": "05129d9571d833ef1980",
     "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "b493c541b54faab120848c4106712d55",
+    "url": "/static/media/about-lisa-siva-800x800.b493c541.png"
   },
   {
     "revision": "e0d9be7771db7c279864e9bb966ec174",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a87e62d82af5dd725488a83cc4691e81.js"
+  "/precache-manifest.c07accc31cbf0ff7831647b9689a894c.js"
 );
 
 self.addEventListener('message', (event) => {
