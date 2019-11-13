cc69ebae782d43451e5d2387dfce0889719b062c
lisasiva <lisashantisiva@gmail.com>
2019-11-13 11:21:47 -0800

    Updates

---
+++ precache-manifest.3af0b937a594edf9f939cefdef6b4ee7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb93bdf51580fef6efb94cf023226ae",
+    "url": "/witchy-atom/index.html"
+  },
+  {
+    "revision": "f54400477c34d1c5337b",
+    "url": "/witchy-atom/static/css/main.b0d21766.chunk.css"
+  },
+  {
+    "revision": "030b1fdf0634124847ab",
+    "url": "/witchy-atom/static/js/2.48bf6283.chunk.js"
+  },
+  {
+    "revision": "f54400477c34d1c5337b",
+    "url": "/witchy-atom/static/js/main.e84a96b1.chunk.js"
+  },
+  {
+    "revision": "4bdd01dbbcdde7903bc8",
+    "url": "/witchy-atom/static/js/runtime-main.fbe8d7d0.js"
+  },
+  {
+    "revision": "3c3794a5db5d1ea46a3a96b87af29dec",
+    "url": "/witchy-atom/static/media/about-lisa-siva-claudia-goetzelmann.3c3794a5.png"
+  },
+  {
+    "revision": "e0d9be7771db7c279864e9bb966ec174",
+    "url": "/witchy-atom/static/media/icon-crystal-ball.e0d9be77.svg"
+  },
+  {
+    "revision": "824265dff362434ce6aea5ee376e222a",
+    "url": "/witchy-atom/static/media/icon-crystal.824265df.svg"
+  },
+  {
+    "revision": "09ca174e23c113a3a45c49352f86a306",
+    "url": "/witchy-atom/static/media/icon-spell-book.09ca174e.svg"
+  },
+  {
+    "revision": "cccf3cf5f5a4bfd151014ee929337ea4",
+    "url": "/witchy-atom/static/media/icon-swords.cccf3cf5.svg"
+  },
+  {
+    "revision": "f169f94803be8266d4b72ecaa857f702",
+    "url": "/witchy-atom/static/media/icon-wand.f169f948.svg"
+  },
+  {
+    "revision": "ff2d9487362888934103baeb1b47210c",
+    "url": "/witchy-atom/static/media/icon-witch-hat.ff2d9487.svg"
+  },
+  {
+    "revision": "c346ba37b5c109a3044b45e2b869e171",
+    "url": "/witchy-atom/static/media/project-conway-game-of-life.c346ba37.png"
+  },
+  {
+    "revision": "26de92add91a4fe42546497a9c02b131",
+    "url": "/witchy-atom/static/media/project-heyhon.26de92ad.png"
+  },
+  {
+    "revision": "e9acf8586426cbdab4bad84059f4f5b4",
+    "url": "/witchy-atom/static/media/project-personal-website-lisa-siva.e9acf858.png"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/witchy-atom/static/media/social-github-lisa-siva.ec5bc1b3.svg"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/witchy-atom/static/media/social-linkedin-lisa-siva.1e01f0c0.svg"
+  }
+]);
